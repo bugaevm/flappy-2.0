19ee830e66d24512196a3722e8044a1880a057e7
yarubbsourigh <55441776+yarubbsourigh@users.noreply.github.com>
2019-11-26 15:15:42 +0300

    Update colors.py

--- colors.py
+++ colors.py
@@ -3,6 +3,4 @@ def rgb2html(r,g,b):
 def html2rgb(html):
     return int(html[1:3],base=16),int(html[3:5],base=16),int(html[5:7],base=16)
 def grad(colorA,colorB,alpha):
-    return colorA[0]*(1-alpha)+colorB[0]*alpha,\
-        colorA[1]*(1-alpha)+colorB[1]*alpha,\
-        colorA[2]*(1-alpha)+colorB[2]*alpha
+    return tuple(colorA[i]*(1-alpha)+colorB[i]*alpha for i in range(3))
