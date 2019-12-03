02e9be424fe439559570ab0ea83fabbddd0a85ca
bugaevm <bugaev.ma@phystech.edu>
2019-12-03 12:52:20 +0300

    to pep8

--- colors.py
+++ colors.py
@@ -1,6 +1,10 @@
-def rgb2html(r,g,b):
+def rgb2html(r, g, b):
     return "#"+format(r,'02x')+format(g,'02x')+format(b,'02x')
+
 def html2rgb(html):
-    return int(html[1:3],base=16),int(html[3:5],base=16),int(html[5:7],base=16)
-def grad(colorA,colorB,alpha):
-    return tuple(colorA[i]*(1-alpha)+colorB[i]*alpha for i in range(3))
+    return tuple(
+        int(html[1:3], base=16), int(html[3:5], base=16), int(html[5:7], base=16)
+    )
+
+def grad(colorA, colorB, alpha):
+    return tuple(colorA[i] * (1 - alpha) + colorB[i] * alpha for i in range(3))
